e677446f546cf921150fc49a9f4ab08e1c77dfb6
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2024-03-12 22:26:32 -0300

    feat: create generic SearchBar component

--- src/modules/customers/views/CustomersView/StandaloneView.vue
+++ src/modules/customers/views/CustomersView/StandaloneView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import AppFeatureHeader from '@/components/AppFeatureHeader.vue';
+import SearchBar from '@/components/forms/SearchBar.vue';
 import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
 </script>
 
 <template>
@@ -11,12 +11,7 @@ import InputText from 'primevue/inputtext';
     :has-tabs="true"
   >
     <template #actions>
-      <span class="relative">
-        <i
-          class="fa fa-magnifying-glass absolute top-2/4 -mt-2 left-3 text-surface-400 dark:text-surface-600"
-        />
-        <InputText type="text" placeholder="Pesquisar..." class="pl-10 w-44" size="small" />
-      </span>
+      <SearchBar />
       <Button label="Adicionar" icon="fa-solid fa-plus" size="small" />
     </template>
   </AppFeatureHeader>

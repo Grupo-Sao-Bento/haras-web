8fd2afad7d0d5e776c746640948af6682c8ea05b
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2024-03-14 19:18:20 -0300

    feat: create generic FormControl component

--- src/modules/home/views/HomeView.vue
+++ src/modules/home/views/HomeView.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import FormControl from '@/components/forms/FormControl.vue';
+</script>
+
 <template>
-  <h1>Bem vindo!</h1>
+  <!-- <h1>Bem vindo!</h1> -->
+  <FormControl label="Label example" type="password" id="test" placeholder="test placeholder" />
 </template>

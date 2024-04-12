104df50dad374f5c2ce4add7d303cf3d447599cc
nicolasgandrade <nicolasguerrero72@gmail.com>
2024-04-11 22:37:41 -0300

    feat: create base form layout

---
+++ src/modules/customers/components/CustomerForm.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import FormControl from '@/components/forms/FormControl.vue';
+</script>
+
+<template>
+  <div class="gap-4 grid grid-cols-3 mb-4">
+    <FormControl
+      label="Nome*"
+      id="customer-name"
+      name="customer-name"
+      type="text"
+      placeholder="Primeiro nome"
+      classes="col-span-1"
+    />
+
+    <FormControl
+      label="Sobrenome*"
+      id="customer-last-name"
+      name="customer-last-name"
+      type="text"
+      placeholder="Sobrenome"
+      classes="col-span-2"
+    />
+  </div>
+
+  <div class="gap-4 flex mb-4">
+    <FormControl
+      label="Telefone*"
+      id="customer-phone-number"
+      name="customer-phone-number"
+      type="tel"
+      placeholder="(10) 98765-4321"
+      classes="w-full"
+    />
+
+    <FormControl
+      label="Email*"
+      id="customer-email"
+      name="customer-email"
+      type="email"
+      placeholder="exemplo@email.com"
+      classes="w-full"
+    />
+  </div>
+
+  <div class="gap-4 flex wb-4">
+    <FormControl
+      label="CPF"
+      id="customer-cpf"
+      name="customer-cpf"
+      type="text"
+      placeholder="000.000.000-00"
+      classes="w-full"
+    />
+
+    <FormControl
+      label="CNPJ"
+      id="customer-cnpj"
+      name="customer-cnpj"
+      type="email"
+      placeholder="00.000.000/0001-00"
+      classes="w-full"
+    />
+  </div>
+</template>
--- src/modules/customers/views/CustomersView/StandaloneView.vue
+++ src/modules/customers/views/CustomersView/StandaloneView.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 import AppFeatureHeader from '@/components/AppFeatureHeader.vue';
+import AppFullScreenModal from '@/components/AppFullScreenModal.vue';
+import AppFullScreenModalCard from '@/components/AppFullScreenModalCard.vue';
 import SearchBar from '@/components/forms/SearchBar.vue';
 import Button from 'primevue/button';
+
+import CustomerForm from '../../components/CustomerForm.vue';
+
+const modalHeader = ref('Adicionar cliente');
+const modalVisible = ref(false);
+
+function closeModal() {
+  modalVisible.value = false;
+}
 </script>
 
 <template>
@@ -12,7 +25,20 @@ import Button from 'primevue/button';
   >
     <template #actions>
       <SearchBar />
-      <Button label="Adicionar" icon="fa-solid fa-plus" />
+      <Button label="Adicionar" icon="fa-solid fa-plus" @click="modalVisible = true" />
     </template>
   </AppFeatureHeader>
+
+  <AppFullScreenModal
+    :headerTitle="modalHeader"
+    :visible="modalVisible"
+    :isPrimaryButtonDisabled="false"
+    @hide="closeModal"
+  >
+    <template #modalContent>
+      <AppFullScreenModalCard>
+        <CustomerForm />
+      </AppFullScreenModalCard>
+    </template>
+  </AppFullScreenModal>
 </template>
